629685549a046dc11cc3a583d183589ae0893089
Abrahm Routhier <ajrouthier2505@outlook.com>
2024-02-20 15:39:59 -0500

    Win Game and Buttons Fix
    Game can now be won!! and Buttons work on end screen

---
+++ ARouthier-Platformer/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other){
+        SceneManager.LoadScene("EndScene");
+    }
+}
